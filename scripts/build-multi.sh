78f73f477931b225a323da0af4fb83c26f23bfda
Julien Bisconti <veggiemonk@users.noreply.github.com>
2022-04-10 09:27:56 +0000

    adding scripts

---
+++ scripts/build-multi.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail # see https://gist.github.com/mohanpedala/1e2ff5661761d3abd0385e8223e16425
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
+pushd "$ROOT_DIR" || { echo "could not change to root directory: $ROOT_DIR"; exit 1; }
+
+
+docker build -t srvmulti -f multi.Dockerfile . 
